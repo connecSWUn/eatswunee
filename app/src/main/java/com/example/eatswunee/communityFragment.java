24d261dc85433a3e38678d3198688f771b508323
jurizuri <angelazooa@gmail.com>
2023-03-30 22:03:34 +0900

    .

--- app/src/main/java/com/example/eatswunee/communityFragment.java
+++ app/src/main/java/com/example/eatswunee/communityFragment.java
@@ -7,15 +7,46 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class communityFragment extends Fragment {
 
+    //사용할 컴포넌트 선언
+    Button reg_button;
+    EditText title_et, content_et;
+
+    // 유저아이디 변수
+    String userid = "";
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_community);
 
+        // ListActivity 에서 넘긴 userid 를 변수로 받음
+        userid = getIntent().getStringExtra("userid");
+
+        // 컴포넌트 초기화
+        title_et = findViewById(R.id.title_et);
+        content_et = findViewById(R.id.content_et);
+        reg_button = findViewById(R.id.reg_button);
+
+        // 버튼 이벤트 추가
+        reg_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 게시물 등록 함수
+                RegBoard regBoard = new RegBoard();
+                regBoard.execute(userid, title_et.getText().toString(), content_et.getText().toString());
+            }
+        });
     }
+
+    private void setContentView(int fragment_community) {
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
